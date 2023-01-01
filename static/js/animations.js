428c7a6e93a69590c90bc15311d2127c9e1ffcb2
chrooks <brooks.ch@northeastern.edu>
2023-01-01 02:02:16 -0500

    style changes (need to do a better job of organizing these)

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,3 +1,13 @@
+gsap.registerPlugin(ScrollTrigger);
+
 console.log("Testing JS functionality");
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray("experience-body").array.forEach(experienceBody => {
+    let hover = gsap.to(experienceBody, {
+        duration: 1,
+        color: "blue"
+    });
+    experienceBody.addEventListener('mouseenter', () => hover.play())
+    experienceBody.addEventListener('mouseleave', () => hover.reverse())
+    console.log("Hover event registered to experience body")
+});
